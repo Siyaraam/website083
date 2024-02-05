ccfae3914de4e92f91ecf8c9a5c24d7b080f8bb5
siyaraam <siyaraam80@gmail.com>
2024-02-05 16:44:47 +0530

    my first code

---
+++ hardtoresistdeal.js
@@ -0,0 +1,6 @@
+export default [
+    
+  "https://images-static.nykaa.com/uploads/5583d54f-3bbd-4e0c-aa80-5a926fc6cedf.jpg?tr=w-1200,cm-pad_resize",
+"https://images-static.nykaa.com/uploads/9a78c1e3-0cf1-444a-889a-e8ecba0ab5c9.jpg?tr=w-1200,cm-pad_resize",
+]
+
